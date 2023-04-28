9e0700c5ea81ae7049b0e9eaee9147b2456ded41
blee241 <128437170+blee241@users.noreply.github.com>
2023-04-28 16:44:00 -0700

    fixed bug causing undefined to appear in usage section when no license was chosen

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -55,13 +55,15 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license, username) {
+  let output = '';
   if (license != 'None') {
-  return `## License
+    output = `## License
 
-  Copyright (c) ${username}. All rights reserved. <br>
-  Licensed under the [${license}](${renderLicenseLink(license)}) license.
-  `
-  }
+    Copyright (c) ${username}. All rights reserved. <br>
+    Licensed under the [${license}](${renderLicenseLink(license)}) license.
+    `;
+  };
+  return output;
 }
 
 // TODO: Create a function to generate markdown for README
